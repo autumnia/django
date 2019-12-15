e2cc03bd8dc0a6c8f4bb32ba97df786995dbe720
autumnia <autumnya@gmail.com>
2019-12-15 12:46:57 +0900

    My DjangoGirls app first commit

---
+++ blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from blog.models import Post
+
+# Create your views here.
+def post_list(request):
+    qs = Post.objects.all()
+    qs = qs.filter(published_date__lte=timezone.now())
+    qs = qs.order_by('published_date')
+    return render( request, 'blog/post_list.html', 
+        {'post_list': qs,}
+    )
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    return render(request, 'blog/post_detail.html',
+        {'post': post,}
+    )
